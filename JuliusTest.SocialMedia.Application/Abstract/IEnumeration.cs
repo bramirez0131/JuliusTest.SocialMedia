44d71483f73c4f95bfa74835718b4a8a646548be
Brahiam Andrey Ramirez Ramirez <brahiam.ramirez@softwareestrategico.com>
2020-09-29 20:22:53 -0500

    Implementación base de Aplicación y Presentación

---
+++ JuliusTest.SocialMedia.Application/Abstract/IEnumeration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace JuliusTest.SocialMedia.Application.Abstract
+{
+	/// <summary>
+	/// Interface IEnumeration
+	/// Implements the <see cref="System.IComparable" />
+	/// </summary>
+	/// <seealso cref="System.IComparable" />
+	public interface IEnumeration : IComparable
+	{
+	}
+}
